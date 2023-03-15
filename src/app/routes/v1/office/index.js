c8151239c26f8718fee4f8abbdf1282291fd07f6
MominThabet <momin.thabet@gmail.com>
2023-03-15 07:16:49 +0200

    using auth in office

--- src/app/routes/v1/office/index.js
+++ src/app/routes/v1/office/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const {
+  isAuthenticated,
+  isAddressApproved,
+  isAuthVerified,
+  isTeamAuth,
+} = require('../../../../utils/middleware/auth/auth');
 // const { validationResult } = require('express-validator');
 const controller = require('../../../controller/office');
 // const { addOffice } = require('../../../validationSchema/office');
 
 const router = express.Router();
 
-router.post('/', controller.addOffice);
+router.use(isAuthenticated, isAuthVerified, isAddressApproved);
 router.get('/', controller.getAllOffices);
+router.use(isTeamAuth);
+router.post('/', isTeamAuth, controller.addOffice);
+// TODO: add edit & delete for team users on office no needed now
+
 module.exports = router;
